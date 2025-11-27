0fa90f14ca5bf3f3a9aa3a6d4ec5312d8b9560f8
Mayur BHIV <blackholeinfiverse84@gmail.com>
2025-11-27 16:06:01 +0530

    Apply theme toggle to public/auth layouts

--- src/components/public/PublicLayout.jsx
+++ src/components/public/PublicLayout.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import PublicNav from './PublicNav'
 import PublicFooter from './PublicFooter'
+import ThemeToggle from '../ThemeToggle'
 import './Public.css'
 
 const PublicLayout = () => {
   return (
     <div className="public-layout">
       <PublicNav />
+      <div className="public-theme-toggle">
+        <ThemeToggle />
+      </div>
       <main className="public-main">
         <Outlet />
       </main>
